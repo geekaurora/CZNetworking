1fd8ca2aaa7fb25f54b29219051c8f3d40ca97d7
Cheng Zhang <showt2me@gmail.com>
2017-12-24 23:04:45 -0800

    Reorg project folders

---
+++ Example/CZNetworkingDemo/CZNetworkingDemo-bridging-header.h
@@ -0,0 +1,14 @@
+//
+//  CZNetworkingDemo-bridging-header.h
+//  CZNetworkingDemo
+//
+//  Created by Cheng Zhang on 12/24/17.
+//  Copyright © 2017 Cheng Zhang. All rights reserved.
+//
+
+#ifndef CZNetworkingDemo_bridging_header_h
+#define CZNetworkingDemo_bridging_header_h
+
+//#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* CZNetworkingDemo_bridging_header_h */
---
+++ Example/CZNetworkingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CZNetworkingDemo
+//
+//  Created by Cheng Zhang on 12/24/17.
+//  Copyright © 2017 Cheng Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
