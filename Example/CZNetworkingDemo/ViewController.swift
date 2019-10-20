4f1a6d184491deabc62092d826a8f042f8f29788
Cheng Zhang <showt2me@gmail.com>
2019-10-20 12:43:24 +0900

    [Update] Minor update of Demo

--- Example/CZNetworkingDemo/ViewController.swift
+++ Example/CZNetworkingDemo/ViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import CZNetworking
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let _ = CZHTTPManager.shared
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
