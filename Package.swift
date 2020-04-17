17104a31ad54679517b4d5502d73bbc366e66c1e
Cheng Zhang <showt2me@gmail.com>
2020-04-18 03:08:28 +0900

    Update CZUtils lib with md5.

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CZNetworking",
-    platforms: [
-        .iOS(.v11),
-    ],
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -17,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/geekaurora/CZUtils.git", from: "3.0.4")
+        .package(url: "https://github.com/geekaurora/CZUtils.git", from: "3.1.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/CZNetworking/Utils/Extensions.swift
+++ Sources/CZNetworking/Utils/Extensions.swift
@@ -8,25 +8,6 @@
 
 import Foundation
 import CZUtils
-import CommonCrypto
-
-public extension String {
-    /// MD5 HashValue: Should #import <CommonCrypto/CommonCrypto.h> in your Bridging-Header file
-    public var MD5: String {
-        let length = Int(CC_MD5_DIGEST_LENGTH)
-        var digest = [UInt8](repeating: 0, count: length)
-        
-        if let d = self.data(using: String.Encoding.utf8) {
-            _ = d.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
-                CC_MD5(body, CC_LONG(d.count), &digest)
-            }
-        }
-        
-        return (0..<length).reduce("") {
-            $0 + String(format: "%02x", digest[$1])
-        }
-    }
-}
 
 public extension Data {
     public mutating func append(_ string: String, using encoding: String.Encoding = .utf8) {
