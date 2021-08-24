383562cea33cb0adce17739cd20c7d7b94496de2
Cheng Zhang <showt2me@gmail.com>
2021-08-24 16:54:45 +0900

    Stub: response.

--- Sources/CZNetworking/StubSupport/URLProtocolMock.swift
+++ Sources/CZNetworking/StubSupport/URLProtocolMock.swift
@@ -4,7 +4,7 @@ import CZUtils
 /**
  Mock that be used to inject data into default`URLSession` with `URLSessionConfiguration`.
  
- - Note: Return mockData for `url` if exists,  otherwise will let other URLProtocols to handle.
+ - Note: Return mockData for `url` if exists,  otherwise it will let other URLProtocols to handle.
  */
 public class URLProtocolMock: URLProtocol {
   // TestData for url - [testURL: testData].
@@ -22,17 +22,19 @@ public class URLProtocolMock: URLProtocol {
     return false
   }
   
-  // TODO: Check url of URLSessionTask.
-  // public override class func canInit(with task: URLSessionTask) -> Bool {}
-  
   // MARK: - Load data
   
   /** Return mockData for `url` if exists,  otherwise call `super.startLoading()`.  */
   public override func startLoading() {
-    // Return mockData for `url` if exists.
+    //  Check whether `url` exists in `mockDataMap`.
     if let url = request.url,
        let data = Self.mockDataMap[url] {
+      
+      // Return mockData for `url`.
       self.client?.urlProtocol(self, didLoad: data)
+      
+      // let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+      // self.client?.urlProtocol(self, didReceive: response)
       
       // Mark as finished.
       self.client?.urlProtocolDidFinishLoading(self)
@@ -44,8 +46,11 @@ public class URLProtocolMock: URLProtocol {
   }
   
   public override func stopLoading() {}
-    
+  
   public override class func canonicalRequest(for request: URLRequest) -> URLRequest {
     return request
   }
+  
+  // TODO: Check url of URLSessionTask.
+  // public override class func canInit(with task: URLSessionTask) -> Bool {}
 }
