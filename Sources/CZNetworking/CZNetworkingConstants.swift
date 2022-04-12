6bb1359e2903ab2a0ddddff95bffaa6a48295e52
Cheng Zhang <showt2me@gmail.com>
2022-04-12 23:48:22 +0900

    shouldJoinOnFlightOperation = false

--- Sources/CZNetworking/CZNetworkingConstants.swift
+++ Sources/CZNetworking/CZNetworkingConstants.swift
@@ -5,5 +5,5 @@ import Foundation
  */
 public enum CZNetworkingConstants {
   /// Indicates whether to join the on-flight GET operation. Default to false.
-  public static var shouldJoinOnFlightOperation = true
+  public static var shouldJoinOnFlightOperation = false
 }
