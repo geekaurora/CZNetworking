38fe40e344c295901568ef08e9f4662347f78de1
cheng zhang <showt2me@gmail.com>
2023-04-13 18:29:40 -0700

    ok

--- Example/CZNetworkingDemo/ViewController.swift
+++ Example/CZNetworkingDemo/ViewController.swift
@@ -4,7 +4,7 @@ import CZNetworking
 
 class ViewController: UIViewController {
   
-  static let endpoint = "https://jsonplaceholder.typicode.com/posts"
+  static let endpoint = "https://www.bluharborbywindsor.com/floorplans/a2-w"
   private let httManager: CZHTTPManager = CZHTTPManager.shared.maxConcurrencies(1)
   
   override func viewDidLoad() {
@@ -15,19 +15,29 @@ class ViewController: UIViewController {
   }
   
   func testFetch() {
-    (0..<5).forEach { _ in
+    (0..<1).forEach { _ in
       self.fetchFeeds()
     }
   }
   
   /// Fetch codable Feed array with json data.
   func fetchFeeds() {
-    httManager.GETCodableModel(
+    httManager.GET(
       Self.endpoint,
-      success: { (feeds: [Feed], data) in
-        dbgPrint("Succeed to fetch feeds: \n\(feeds.map { $0.id })")
+      success: { data in
+        let html_string =  String(data: data!, encoding: .utf8)
+        print("html_string: \(html_string!)")
+
       }, failure: { (error) in
         assertionFailure("Failed to fetch feeds. Error - \(error)")
       })
+
+//    httManager.GETCodableModel(
+//      Self.endpoint,
+//      success: { (feeds: [Feed], data) in
+//        dbgPrint("Succeed to fetch feeds: \n\(feeds.map { $0.id })")
+//      }, failure: { (error) in
+//        assertionFailure("Failed to fetch feeds. Error - \(error)")
+//      })
   }
 }
